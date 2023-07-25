e727f48311b79d311d83f8db724b57ede6005727
ihsajjad <ihsajjad1@gmail.com>
2023-07-26 02:52:26 +0600

    problem 7 is solved

--- task-7.js
+++ task-7.js
@@ -1,1 +1,30 @@
 // Task 7: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
+
+const romanToInt = (roman) => {
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+    const currentVal = romanNumerals[roman[i]];
+    const nextVal = romanNumerals[roman[i + 1]];
+
+    if (nextVal && currentVal < nextVal) {
+      result -= currentVal;
+    } else {
+      result += currentVal;
+    }
+  }
+
+  return result;
+};
+
+console.log(romanToInt("XXI")); // Output: 21
--- task-8.js
+++ task-8.js
@@ -1,1 +1,20 @@
 // Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
+
+const array = [1, 3, 4, 5, 9];
+
+const findSecondSmallestNum = (array) => {
+  let secondSmallestNum = 0;
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+    secondSmallestNum = element;
+    console.log(element);
+    if (secondSmallestNum > element) {
+      secondSmallestNum = element;
+    } else {
+    }
+  }
+  console.log(secondSmallestNum);
+};
+
+findSecondSmallestNum(array);
